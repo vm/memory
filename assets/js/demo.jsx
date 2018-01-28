0fad41287205ba2111072878f7fe6d559fd7cb94
Vignesh Mohankumar <vmohankumar@driftt.com>
2018-01-27 20:48:37 -0500

    done

--- assets/js/demo.jsx
+++ assets/js/demo.jsx
@@ -1,50 +1,105 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button } from 'reactstrap';
+import _ from 'lodash/fp'
 
 export default function run_demo(root) {
-  ReactDOM.render(<Demo side={0}/>, root);
+  ReactDOM.render(<Demo/>, root);
 }
 
 class Demo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { side: props.side };
+    this.state = {
+      clicks: 0,
+      letters: this.randomizeLetters(),
+      completed: [],
+      checkIndex: undefined,
+      confirmIndex: undefined,
+    };
+    this.toggle = this.toggle.bind(this);
+    this.afterToggle = this.afterToggle.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
-  toggle(side) {
-    var side = +!this.state.side;
-    this.setState({side: side});
+  reset() {
+    this.setState({
+      clicks: 0,
+      completed: [],
+      checkIndex: undefined,
+      confirmIndex: undefined,
+      letters: this.randomizeLetters(),
+    })
+  }
+
+  randomizeLetters() {
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    return _.shuffle(letters.concat(letters));
+  }
+
+  afterToggle() {
+    const { confirmIndex, checkIndex, letters, completed } = this.state;
+    const interval = setInterval(() => {
+      this.setState({
+        completed: letters[confirmIndex] === letters[checkIndex]
+          ? completed.concat([letters[checkIndex]])
+          : completed,
+        checkIndex: undefined,
+        confirmIndex: undefined,
+      });
+    }, 1000);
+    setTimeout(() => clearInterval(interval), 1000)
+  }
+
+  toggle(i) {
+    const { clicks, confirmIndex, checkIndex } = this.state;
+    if (_.isUndefined(checkIndex)) {
+      this.setState({
+        checkIndex: i,
+        clicks: clicks + 1,
+      })
+    } else if (_.isUndefined(confirmIndex)) {
+      this.setState({
+        confirmIndex: i,
+        clicks: clicks + 1,
+      }, this.afterToggle);
+    }
   }
 
   render() {
-    var toggle = this.toggle.bind(this);
+    const { letters, clicks, completed, checkIndex, confirmIndex } = this.state;
     return (
-      <div className="row">
-        <Side show={this.state.side == 0} toggle={toggle} />
-        <div className="col">
-          &nbsp;
+      <div className="memory-game">
+        <div>clicks {clicks}</div>
+        <Button onClick={this.reset}>Reset</Button>
+        <div className="memory-game-grid">
+          {this.state.letters.map((letter, i) =>
+            <Side
+              letter={letter}
+              key={i}
+              i={i}
+              visible={checkIndex === i || confirmIndex === i}
+              completed={completed.includes(letter)}
+              toggle={this.toggle}
+            />
+          )}
         </div>
-        <Side show={this.state.side == 1} toggle={toggle} />
       </div>
     );
   }
 }
 
-function Side(params) {
-  if (params.show) {
-    return (
-      <div id="side-0" className="side col" onMouseOver={ () => params.toggle() }>
-        <Button onClick={ () => alert("cheater") }>Click Me</Button>
-      </div>
-    );
-  }
-  else {
-    return (
-      <div id="side-0" className="side col">
-        &nbsp;
-      </div>
-    );
-  }
+function Side(props) {
+  const { visible, completed, i, letter, toggle } = props
+  return (
+    <div className="memory-game-tile">
+      {visible
+        ? letter
+        : completed
+          ? 'complete'
+          : <Button onClick={() => toggle(i)}>Click Me</Button>
+      }
+    </div>
+  )
 }
 
